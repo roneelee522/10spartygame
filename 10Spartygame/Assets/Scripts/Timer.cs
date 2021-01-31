a4136f986dbfed5bc7c8ce6336d014c0e7fd1d4e
roneelee522 <roneelee522@hotmail.com>
2021-01-30 20:56:14 -0500

    main mehanic

---
+++ 10Spartygame/Assets/Scripts/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timer = 0f;
+    Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = "Timer: " + Mathf.Round(timer);
+        if(timer >= 10f)
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ 10Spartygame/Assets/Scripts/carcontoller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class carcontoller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float carAcceleration;
+    public Text resultText;
+
+    private float carSpeed;
+    private Rigidbody2D car;
+
+    private float timer = 0f;
+
+
+
+    void Start()
+    {
+        car = GetComponent<Rigidbody2D>();
+        resultText.text = "";
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            carSpeed = carSpeed + carAcceleration * Time.deltaTime;
+        }
+
+        car.velocity = transform.up * carSpeed;
+
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= 10.0f)
+        {
+            resultText.text = "You Failed!";
+            Destroy(this);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            resultText.text = "Goal!";
+            Destroy(this);
+        }
+    }
+    
+
+}
